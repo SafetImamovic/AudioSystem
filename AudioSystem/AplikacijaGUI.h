99e1573c893535eed036df317a1fb945301fd9c4
Hamza9199 <hamza.gacic.22@size.ba>
2023-12-12 19:39:36 +0100

    fix

--- AudioSystem/AplikacijaGUI.h
+++ AudioSystem/AplikacijaGUI.h
@@ -1,5 +1,58 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+
+#include "TextBox.h"
+#include "Tipka.h"
+
+/*
+	Klasa koja je odgovorna za GUI
+*/
+
 class AplikacijaGUI
 {
-};
+private:
+	sf::RenderWindow* window; //sf::RenderWindow definise prozor za GUI
+	sf::Event event; //event je objekat koji moze da poprimi vrijednost ili vrstu eventa koji se je desio unutar prozora,
+	//cak i van prozora ako je tako definisano
+	sf::VideoMode videoMode; //zadaje rezoluciju prozoru
+	sf::Vector2i mousePozProzor; //cuva poziciju misa relativnu prozoru (koordinate)
+	sf::Font font;
+	TextBox textbox1;
+	Tipka tipka1;
 
+	//elementi aplikacije
+	//tok definiranja, logike i iscrtavanja elemenata na prozor je sljedeci:
+	//1. deklaracija elementa ovdje
+	//2. inicijalizacija elementa koja ce se na kraju pozvat u konstruktoru (u ovom slucaju se karakteristike elementa definiraju unutar private
+	// metode InicijalizacijaElemenata() pa se ona poziva unutar konstruktora)
+	//3. osvjezavanje elemenata ili "update" logike elemenata npr. u zavisnoti od events kao klikanje na element itd.
+	//na kraju se uvijek poziva u funkciji UpdateGUI(); moze se prije definirati logika unutar neke druge metode pa onda ona pozvati u UpdateGUI()
+	//4. iscrtavanje ili render na ekran koja se na kraju poziva u RenderGUI(); moze imati svoju metodu kao i prije
+	//Bitno je znati da render ima svoj redoslijed npr. ako je neki element koji je zadnji pozvan, on ce se iscrtati na vrhu svih koji 
+	// su bili pozvani prije njega 
+	//unutar metode posebne za element je potrebno pozvat this->window->draw(this->(element))
+
+	sf::RectangleShape rect; //pravougaonik
+
+	void InicijalizacijaVarijabli();
+	void InicijalizacijaProzora();
+	void InicijalizacijaElemenata();
+
+public:
+	AplikacijaGUI(sf::Font& font);
+	~AplikacijaGUI();
+
+	void UpdateRect();
+	void UpdatePozicijaMisa();
+	void UpdatePollEvents();
+	void UpdateGUI();
+
+	void RenderRect();
+	void RenderGUI();
+
+	void PromijeniRezoluciju();
+
+	const bool getWindowIsOpen() const;
+};
--- AudioSystem/TextBox.h
+++ AudioSystem/TextBox.h
@@ -1,5 +1,45 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include <sstream>
+#include <iostream>
+
+//definira ascii vrijednost specijalnih tipki
+#define DELETE_KEY 8
+#define ENTER_KEY 13
+#define ESCAPE_KEY 27
+
+/*
+	Klasa odgovorna za textbox, znaci mjesto gdje korisnik moze da kuca tekst
+*/
+
 class TextBox
 {
-};
+private:
+	sf::Text textbox;
+	sf::RectangleShape backgroundShape;
+	std::ostringstream text;
+	bool jeOznacen = false;
+	bool imaLimit = false;
+	int Limit;
 
+	void InputLogika(int karakterIstipkan);
+	void IzbrisiZadnjiKarakter();
+
+
+public:
+	TextBox() = default;
+
+	void SetTextBox(int velicina, sf::Color boja, bool oznacen);
+	void SetFont(sf::Font& font);
+	void SetPosition(sf::Vector2f pozicija);
+	void SetLimit(bool ImaIliNema);
+	void SetLimit(bool ImaIliNema, int Limit);
+	void SetOznacen(bool oznacen);
+	void SetBackground(sf::Color boja, sf::Vector2f velicina);
+	void SetPozadinaPozicija(sf::Vector2f pozicija);
+	void SetSve(int velicinaTeksta, sf::Color bojaTeksta, sf::Color bojaPozadine, bool oznacen, sf::Font& font, sf::Vector2f pozicija, sf::Vector2f padding, sf::Vector2f velicinaPozadine);
+	std::string GetText();
+
+	void DrawTo(sf::RenderWindow& window);
+	void OtipkanoNa(sf::Event input);
+};
